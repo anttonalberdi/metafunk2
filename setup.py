4371ecbf3fe878eb83d95a44ed12346d24cae896
Antton Alberdi <37664231+anttonalberdi@users.noreply.github.com>
2019-11-19 06:41:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
+         "License :: OSI Approved :: GPL-3",
          "Operating System :: OS Independent",
      ],
  )
