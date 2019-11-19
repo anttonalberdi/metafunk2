0616d0da2e7bcc66b869e17de7f5f122bfbfeeac
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-19 15:17:26 +0100

    f

--- metafunk2/genome_mapping.py
+++ metafunk2/genome_mapping.py
@@ -8,7 +8,26 @@ import sys
 import random
 import argparse
 import subprocess
+from shutil import copyfile
 
+
+
+#Copy reference genome to working directory
+def copy_genome(refgenomepath):
+    #Create genome_mapping directory if it does not exist
+    newdir = "genome_mapping"
+    absnewdir = os.path.join(outpath, name + '.' + newdir)
+    #Create genome_mapping/genomes directory if it does not exist
+    if not os.path.exists(absnewdir):
+        os.makedirs(absnewdir)
+    genomedir = "genomes"
+    absgenomedir = os.path.join(absnewdir, genomedir)
+    if not os.path.exists(absgenomedir):
+        os.makedirs(absgenomedir)
+    #Copy genome file to genome_mapping/genomes directory
+    copyfile(refgenomepath, absgenomedir)
+
+#Manipulate reference genome
 def check_genome(refgenomepath):
     if refgenpath.endswith('.gz'):
         logfile=open(logfilepath,"a+")
@@ -17,8 +36,12 @@ def check_genome(refgenomepath):
         logfile.close()
         DecompCmd = 'pigz '+refgenpath+''
         subprocess.check_call(DecompCmd, shell=True)
-        refgenomepath = refgenomepath.replace(".gz", "")
-
+        refgenomepathnew = refgenomepath.replace(".gz", "")
+    if refgenpath.endswith('.fasta'):
+        original = os.path.join(absdir, name + '.pair1' + '.truncated')
+        os.rename(read1in,read1out)
+    if refgenpath.endswith('.fa'):
+        os.rename(read1in,read1out)
 def index_genome(refgenomepath):
 
 
