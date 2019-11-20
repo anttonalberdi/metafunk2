a297391892f806d1da61c47bf19b308ce9fe5d68
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-20 15:09:03 +0100

    d

--- metafunk2/binning.py
+++ metafunk2/binning.py
@@ -21,12 +21,25 @@ def binning(outpath,name,logfilepath):
     metabatdir = os.path.join(absnewdir, 'metabat')
     if not os.path.exists(metabatdir):
         os.makedirs(metabatdir)
+    assemblypath = os.path.join(outpath, name + '.assembly','contigs.fasta')
     assemblybampath = os.path.join(outpath,name + '.assembly_mapping', name + '.mapped.bam')
     metabatdepthfile = os.path.join(metabatdir, name + '.depth.txt')
 
     #Generate depth file
+    logfile=open(logfilepath,"a+")
+    current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+    logfile.write("{0} |    Generating depth file from the reads mapped to the assembly \r\n".format(current_time))
+    logfile.close()
     metabatdepthfileCmd = 'jgi_summarize_bam_contig_depths --outputDepth '+metabatdepthfile+' '+assemblybampath+''
     subprocess.check_call(metabatdepthfileCmd, shell=True)
+
+    #Run metabat
+    logfile=open(logfilepath,"a+")
+    current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+    logfile.write("{0} |    Running metabat \r\n".format(current_time))
+    logfile.close()
+    metabatCmd = 'metabat2 -i '+assemblypath+' -a '+metabatdepthfile+' -o '+metabatdir+' -m 1500 -t '+threads+' --unbinned'
+	subprocess.check_call(metabatCmd, shell=True)
 
 
         #maxbinCmd = 'prodigal -p meta -q -i '+contigpath+' -f gff -o '+gffpath+' -a '+faapath+' -d '+fnapath+''
@@ -35,4 +48,4 @@ def binning(outpath,name,logfilepath):
 
         #Run binning
         #subprocess.check_call(maxbinCmd, shell=True)
-        #subprocess.check_call(metabatCmd, shell=True)
+        #
