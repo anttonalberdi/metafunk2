6ba9290bbd61d58f4b3a5a1be877e70344b3374e
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-20 07:14:09 +0100

    f

--- metafunk2/run_metafunk2.py
+++ metafunk2/run_metafunk2.py
@@ -71,25 +71,27 @@ if ( 1 in includesteps and 1 not in skipsteps ):
 # Duplicate removal step
 #####
 
-logfile=open(logfilepath,"a+")
-current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
-logfile.write("{0} | This is metafunk2 starting duplicate removal \r\n".format(current_time))
-logfile.close()
+if ( 2 in includesteps and 2 not in skipsteps ):
+    logfile=open(logfilepath,"a+")
+    current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+    logfile.write("{0} | This is metafunk2 starting duplicate removal \r\n".format(current_time))
+    logfile.close()
 
-from duplicate_removal import duplicate_removal
-duplicate_removal(read1,read2,outpath,name,threads)
+    from duplicate_removal import duplicate_removal
+    duplicate_removal(read1,read2,outpath,name,threads)
 
 #####
 # Mapping against reference genomes
 #####
 
-logfile=open(logfilepath,"a+")
-current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
-logfile.write("{0} | This is metafunk2 starting map reads agains reference genomes \r\n".format(current_time))
-logfile.close()
+if ( 3 in includesteps and 3 not in skipsteps ):
+    logfile=open(logfilepath,"a+")
+    current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+    logfile.write("{0} | This is metafunk2 starting map reads agains reference genomes \r\n".format(current_time))
+    logfile.close()
 
-from genome_mapping import copy_genome
-copy_genome(refgenpath)
+    from genome_mapping import copy_genome
+    copy_genome(refgenpath)
 
 #from genome_mapping import check_genome
 #check_genome(refgenpath)
