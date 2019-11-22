00f3cec2057828b5041fdcf49cbe127aeddab2a6
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-22 16:15:37 +0100

    f

--- metafunk2/run_metafunk2.py
+++ metafunk2/run_metafunk2.py
@@ -20,7 +20,7 @@ optional.add_argument('-t', help="Number of threads", dest="threads", default=8)
 optional.add_argument('-m', help="RAM memory limit", dest="memory", default=250)
 optional.add_argument('-s', help="Skip steps", dest="skipsteps", type=str)
 optional.add_argument('-i', help="Include steps", dest="includesteps", type=str)
-optional.add_argument('-k', help="Keep intermediate files", dest="keep", action='store_true',default=True)
+optional.add_argument('-k', help="Keep intermediate files", dest="keep", action='store_true')
 args = parser.parse_args()
 
 name = args.name
