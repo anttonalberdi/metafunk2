235541d0026413601c57b1af9c4dcf30ee5977cf
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-22 15:34:37 +0100

    d

--- metafunk2/run_metafunk2.py
+++ metafunk2/run_metafunk2.py
@@ -42,7 +42,7 @@ if args.memory is None:
 
 print(args.includesteps)
 print(type(args.includesteps))
-print(tuple(args.includesteps.split(',')))
+print(map(tuple(args.includesteps.split(',')),int))
 
 #Prepare skipsteps
 if args.skipsteps is None:
