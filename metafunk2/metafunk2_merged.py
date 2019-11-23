fa18c4ecd9effd0a156cfee6f2baffd8c146a173
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-23 18:35:10 +0100

    f

--- metafunk2/metafunk2_merged.py
+++ metafunk2/metafunk2_merged.py
@@ -8,13 +8,15 @@ import time
 import gzip
 
 #Argument parsing
-parser = argparse.ArgumentParser(description='Runs metafunk2 pipeline.')
-parser.add_argument('-n', help="Project name", metavar="PROJECTNAME", dest="projectname", required=True)
-parser.add_argument('-p', help="Project path", metavar="PROJECTPATH", dest="projectpath", required=True)
-parser.add_argument('-t', help="Number of threads", metavar="THREADS", dest="threads", default=8, required=False)
-parser.add_argument('-m', help="RAM memory limit", metavar="MEMORY", dest="memory", default=250, required=False)
-parser.add_argument('--skipsteps', help="Skip steps", metavar="SKIPSTEPS", dest="skipsteps", nargs='+', type=int, required=False)
-parser.add_argument('--includesteps', help="Include steps", metavar="INCLUDESTEPS", dest="includesteps", nargs='+', type=int, required=False)
+parser = argparse.ArgumentParser(description='Runs metafunk2 merged pipeline.')
+required = parser.add_argument_group('required arguments')
+optional = parser.add_argument_group('optional arguments')
+required.add_argument('-n', help="Project name", metavar="PROJECTNAME", dest="projectname", required=True)
+required.add_argument('-p', help="Project path", metavar="PROJECTPATH", dest="projectpath", required=True)
+optional.add_argument('-t', help="Number of threads", metavar="THREADS", dest="threads", default=8, required=False)
+optional.add_argument('-m', help="RAM memory limit", metavar="MEMORY", dest="memory", default=250, required=False)
+optional.add_argument('-s', help="Skip steps", dest="skipsteps", type=str)
+optional.add_argument('-i', help="Include steps", dest="includesteps", type=str)
 args = parser.parse_args()
 
 projectname = args.projectname
@@ -30,7 +32,7 @@ if args.memory is None:
 if args.skipsteps is None:
     skipsteps = 0,
 else:
-    skipsteps = tuple(args.skipsteps)
+    skipsteps = tuple([int(x) for x in args.skipstepsx.split(',')])
     if isinstance(skipsteps,int):
         skipsteps = (skipsteps,)
 
@@ -38,7 +40,7 @@ else:
 if args.includesteps is None:
     includesteps = 1,2,3,4,5,6,7,8,9
 else:
-    includesteps = tuple(args.includesteps)
+    includesteps = tuple([int(x) for x in args.includesteps.split(',')])
     if isinstance(includesteps,int):
         includesteps = (includesteps,)
 
@@ -85,7 +87,7 @@ if ( 1 in includesteps and 1 not in skipsteps ):
 if ( 2 in includesteps and 2 not in skipsteps ):
     logfile=open(logfilepath,"a+")
     current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
-    logfile.write("{0} | Metafunk2_merged has started to map reads of different samples to the merged assembly \r\n".format(current_time))
+    logfile.write("{0} | Metafunk2_merged is mapping reads of different samples to the reassembly (merged assemblies) \r\n".format(current_time))
     logfile.close()
 
     from reassembly_mapping import reassembly_mapping
