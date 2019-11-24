1c6e6e9cd864937c0b53e0b26a959be6eaa35ac7
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-24 16:01:37 +0100

    f

--- metafunk2/bin_qc.py
+++ metafunk2/bin_qc.py
@@ -14,7 +14,11 @@ import gzip
 def bin_qc(projectpath,threads,memory,logfilepath):
     bindir = os.path.join(projectpath, 'merged/binning/metabat')
     bindirtemp = os.path.join(projectpath, 'merged/binning/metabat.tmp')
+    if not os.path.exists(bindirtemp):
+        os.makedirs(bindirtemp)
     outputdir = os.path.join(projectpath, 'merged/binning/metabat.checkm')
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
 
     checkmCmd = 'module load anaconda2/4.0.0 && checkm lineage_wf -t '+threads+' --pplacer_threads '+threads+' --tmpdir '+bindirtemp+' '+bindir+' '+outputdir+''
     subprocess.check_call(checkmCmd, shell=True)
