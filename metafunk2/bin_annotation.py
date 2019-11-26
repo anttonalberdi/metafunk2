e0e4ca57aa02b6b15e44341d575c6f0f7dfaa42d
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-26 18:47:23 +0100

    f

--- metafunk2/bin_annotation.py
+++ metafunk2/bin_annotation.py
@@ -18,8 +18,7 @@ def bin_annotation(projectpath,threads,memory,logfilepath):
     bindir = os.path.join(projectpath, 'merged/binning')
     databasedir  = '/home/projects/ku-cbd/people/antalb/databases/CAT_prepare_20190719/2019-07-19_CAT_database'
     taxonomydir = '/home/projects/ku-cbd/people/antalb/databases/CAT_prepare_20190719/2019-07-19_taxonomy/'
-    proteinfile = os.path.join(projectpath, 'merged/binning/dastool/dastool_proteins.faa')
     outbase = os.path.join(binannotdir, 'annotation')
 
-    batCmd = 'module unload tools gcc/5.1.0 && module load tools anaconda3/4.0.0 diamond/0.9.29 prodigal/2.6.3 cat/5.0.3 && CAT bins -b '+bindir+' -s .fa -d '+databasedir+' -t '+taxonomydir+' -p '+proteinfile+' -o '+outbase+' -n '+threads+' --force '
+    batCmd = 'module unload tools gcc/5.1.0 && module load tools anaconda3/4.0.0 diamond/0.9.29 prodigal/2.6.3 cat/5.0.3 && CAT bins -b '+bindir+' -s .fa -d '+databasedir+' -t '+taxonomydir+' -o '+outbase+' -n '+threads+' --force '
     subprocess.check_call(batCmd, shell=True)
