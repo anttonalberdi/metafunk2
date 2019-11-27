2fcbb4d449ed4da377e4bb6009fd83a34b6b826c
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-27 05:43:53 +0100

    g

--- metafunk2/bin_annotation.py
+++ metafunk2/bin_annotation.py
@@ -32,19 +32,19 @@ def bin_annotation(projectpath,threads,memory,logfilepath):
 
     #Run annotation
     batCmd = 'module unload tools gcc/5.1.0 && module load tools anaconda3/4.0.0 diamond/0.9.29 prodigal/2.6.3 cat/5.0.3 && CAT bins -b '+bindir+' -s .fa -d '+databasedir+' -t '+taxonomydir+' -o '+outbase+' -n '+threads+' --force '
-    #subprocess.check_call(batCmd, shell=True)
+    subprocess.check_call(batCmd, shell=True)
 
     #Add names to taxids
     orffile = os.path.join(projectpath, 'merged/binning/annotation/annotation.bin2classification.txt')
     taxfile = os.path.join(projectpath, 'merged/binning/bin.annotation.txt')
     taxonomydir = '/home/projects/ku-cbd/people/antalb/databases/CAT_prepare_20190719/2019-07-19_taxonomy/'
     taxCmd = 'module unload tools gcc/5.1.0 && module load tools anaconda3/4.0.0 diamond/0.9.29 prodigal/2.6.3 cat/5.0.3 && CAT add_names -i '+orffile+' -o '+taxfile+' -t '+taxonomydir+' --only_official'
-    #subprocess.check_call(taxCmd, shell=True)
+    subprocess.check_call(taxCmd, shell=True)
 
     #Sumarize
     summaryfile = os.path.join(projectpath, 'merged/binning/bin.annotation.summary.txt')
     summaryCmd = 'module unload tools gcc/5.1.0 && module load tools anaconda3/4.0.0 diamond/0.9.29 prodigal/2.6.3 cat/5.0.3 && CAT summarise -i '+taxfile+' -o '+summaryfile+''
-    #subprocess.check_call(summaryCmd, shell=True)
+    subprocess.check_call(summaryCmd, shell=True)
 
     ############
     # Functional annotation
