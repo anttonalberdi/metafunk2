0db829a964ed38f3c67ab457a4c24290a9ed2af9
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-27 05:17:24 +0100

    f

--- development.sh
+++ development.sh
@@ -11,12 +11,15 @@ python metafunk2/metafunk2.py -n blank4 -1 metafunk2_test/Blank06092017_1.fastq.
 
 
 workdir="/home/projects/ku-cbd/people/antalb/metafunk2_test"
-xqsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e ${workdir}/indextest.err -o ${workdir}/indextest.out -l nodes=1:ppn=8,mem=20gb,walltime=0:02:00:00 -N indextest -de python metafunk2/metafunk2.py -n blank -1 metafunk2_test/Blank06092017_1.fastq.gz -2 metafunk2_test/Blank06092017_2.fastq.gz -r 'gambusia=databases/GCA_003097735.1_ASM309773v1_genomic.fna.gz,human=databases/GCF_000001405.39_GRCh38.p13_genomic.fna.gz' -o /home/projects/ku-cbd/people/antalb/metafunk2_test -t 8 -m 8 -i 3 -k
+xqsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e ${workdir}/indextest.err -o ${workdir}/indextest.out -l nodes=1:ppn=8,mem=20gb,walltime=0:02:00:00 -N indextest -de python metafunk2/metafunk2.py -n AI0_11b -1 metafunk2_test2/AI0_11b_1.fastq.gz -2 metafunk2_test2/AI0_11b_2.fastq.gz -r 'gambusia=databases/GCA_003097735.1_ASM309773v1_genomic.fna.gz,human=databases/GCF_000001405.39_GRCh38.p13_genomic.fna.gz' -o /home/projects/ku-cbd/people/antalb/metafunk2_test -t 8 -m 8 -i 3 -k
 
 
 xqsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e ${workdir}/pardre2.err -o ${workdir}/pardre.out -l nodes=1:ppn=8,mem=50gb,walltime=0:00:05:00 -N pardre2 -de python metafunk2/metafunk2.py -n AI0_11b_pardre -1 metafunk2_test2/AI0_11b_1.fastq.gz -2 metafunk2_test2/AI0_11b_2.fastq.gz -r 'gambusia=databases/GCA_003097735.1_ASM309773v1_genomic.fna.gz,human=databases/GCF_000001405.39_GRCh38.p13_genomic.fna.gz' -o /home/projects/ku-cbd/people/antalb/metafunk2_test -t 8 -m 8 -i 2 -k
 
 xqsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e ${workdir}/merged.err -o ${workdir}/merged.out -l nodes=1:ppn=8,mem=50gb,walltime=0:00:05:00 -N original -de python metafunk2/metafunk2.py -n AI0_11b_original -1 metafunk2_test2/AI0_11b_1.fastq.gz -2 metafunk2_test2/AI0_11b_2.fastq.gz -r 'gambusia=databases/GCA_003097735.1_ASM309773v1_genomic.fna.gz,human=databases/GCF_000001405.39_GRCh38.p13_genomic.fna.gz' -o /home/projects/ku-cbd/people/antalb/metafunk2_test -t 8 -m 8 -i 2 -k
+
+
+xqsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e ${workdir}/catdb.err -o ${workdir}/catdb.out -l nodes=1:ppn=1,mem=120gb,walltime=0:12:00:00 -N catdb -de tar xvzf databases/CAT_db/CAT_prepare_20190719.tar.gz
 
 
 li metafunk2_test
@@ -90,8 +93,9 @@ xqsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e ${workdir}/AI0_11b.err -o ${
 #merged
 xqsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e ${workdir}/checkm.err -o ${workdir}/checkm.out -l nodes=1:ppn=8,mem=50gb,walltime=0:01:00:00 -N checkm -de python metafunk2/metafunk2_merged.py -n merged2 -p /home/projects/ku-cbd/people/antalb/metafunk2_test2 -t 8 -m 8 -i 4
 
+xqsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e ${workdir}/CAT.err -o ${workdir}/CAT.out -l nodes=1:ppn=8,mem=50gb,walltime=0:06:00:00 -N CAT -de python metafunk2/metafunk2_merged.py -n merged -p /home/projects/ku-cbd/people/antalb/metafunk2_test2 -t 8 -m 8 -i 5
 
 
 rm -rf metafunk2
 git clone https://github.com/anttonalberdi/metafunk2.git
-python metafunk2/metafunk2_merged.py -n merged2 -p /home/projects/ku-cbd/people/antalb/metafunk2_test2 -t 8 -m 8 -i 3
+python metafunk2/metafunk2_merged.py -n merged -p /home/projects/ku-cbd/people/antalb/metafunk2_test2 -t 8 -m 8 -i 5
