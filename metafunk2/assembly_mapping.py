9cdc1dcfadfd739dce831d765770b4478bc6efe4
“anttonalberdi” <anttonalberdi@gmail.com>
2019-11-29 06:31:56 +0100

    f

--- metafunk2/assembly_mapping.py
+++ metafunk2/assembly_mapping.py
@@ -17,8 +17,17 @@ def assembly_mapping(outpath,name,logfilepath,threads):
     if not os.path.exists(absnewdir):
         os.makedirs(absnewdir)
 
+    ###########
+    # 1) DECLARE INPUT AND OUTPUT FILES AND CHECK IF THEY EXIST
+    ###########
+    # The assembly requires two input elements and one output element to be declared
+    # 1) Assembly file, 2) Forward and reverse read files to be mapped to the assembly,
+    # and 3) the output bam file.
+
+    #Declare the assembly file
     assemblypath = os.path.join(outpath, name + '.fna')
-    #Print error and quit if the assembly file does not exist
+
+    #Print error and kill process if the assembly file does not exist
     if not os.path.exists(assemblypath):
         logfile=open(logfilepath,"a+")
         current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
@@ -26,17 +35,37 @@ def assembly_mapping(outpath,name,logfilepath,threads):
         logfile.close()
         os.kill(os.getpid(), signal.SIGSTOP)
 
-    assemblybampath = os.path.join(absnewdir, name + '.mapped.bam')
+    #Declare the read files
     read1in = os.path.join(outpath, name + '.1.fq')
     read2in = os.path.join(outpath, name + '.2.fq')
+    #Print error and kill process if the read files do not exist
+    if (not os.path.exists(read1in) or not os.path.exists(read2in)):
+        ## Print to log
+        logfile=open(logfilepath,"a+")
+        current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+        logfile.write("{0} | ERROR! Read files of sample {1} do not exist. \r\n".format(current_time,name))
+        logfile.close()
+        ## Kill the job
+        os.kill(os.getpid(), signal.SIGSTOP)
+        ##
 
-    #Index assembly
+    #Declare the output bam file
+    assemblybampath = os.path.join(absnewdir, name + '.mapped.bam')
+
+    ###########
+    # 2) CHECK WHETHER THE ASSEMBLY IS INDEXED AND OTHERWISE INDEX IT
+    ###########
+    # The assembly needs to be indexed in order the reads to be mapped to it.
+
+    #Run the indexing only if the .fai file does not exist
     assemblyfai = os.path.join(assemblypath + '.fai')
     if not os.path.exists(assemblyfai):
+        ## Print to log
         logfile=open(logfilepath,"a+")
         current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
         logfile.write("{0} |    Indexing metagenomic assembly \r\n".format(current_time))
         logfile.close()
+        ##
         samtoolsindexCmd = 'module load tools samtools/1.9 && samtools faidx '+assemblypath+''
         bwaindexCmd = 'module load tools bwa/0.7.15 && bwa index '+assemblypath+''
         subprocess.check_call(samtoolsindexCmd, shell=True)
@@ -47,12 +76,16 @@ def assembly_mapping(outpath,name,logfilepath,threads):
         logfile.write("{0} |    Metagenomic assembly is already indexed\r\n".format(current_time))
         logfile.close()
 
-    #Declare mapping commands
-    mapCmd = 'module load tools samtools/1.9 bwa/0.7.15 && bwa mem -t '+threads+' -R "@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample" '+assemblypath+' '+read1in+' '+read2in+' | samtools view -T '+assemblypath+' -b - | samtools sort -T '+assemblypath+' - > '+assemblybampath+''
+    ###########
+    # 3) MAP THE READS TO THE ASSEMBLY
+    ###########
 
-    #Mapping to genome
+    ## Print to log
     logfile=open(logfilepath,"a+")
     current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
-    logfile.write("{0} |    Mapping reads to assembly \r\n".format(current_time))
+    logfile.write("{0} |    Mapping reads to {1} assembly \r\n".format(current_time,name))
     logfile.close()
+    ##
+
+    mapCmd = 'module load tools samtools/1.9 bwa/0.7.15 && bwa mem -t '+threads+' -R "@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample" '+assemblypath+' '+read1in+' '+read2in+' | samtools view -T '+assemblypath+' -b - | samtools sort -T '+assemblypath+' - > '+assemblybampath+''
     subprocess.check_call(mapCmd, shell=True)
