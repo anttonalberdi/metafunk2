2929de5bf89f9e33d5efeaf02eb3b4e115c9d05b
“anttonalberdi” <anttonalberdi@gmail.com>
2019-12-01 08:44:23 +0100

    d

--- metafunk2/assembly.py
+++ metafunk2/assembly.py
@@ -40,9 +40,9 @@ def assembly(outpath,name,logfilepath,statsfilepath,threads,memory,keep,assemble
 
     #Select assembler
     if assembler == 'spades':
-        assemblyCmd = 'module load anaconda3/2.1.0 spades/3.13.1 perl/5.20.2 && metaspades.py -1 '+read1in+' -2 '+read2in+' -t '+threads+' -m '+memory+' -k 21,29,39,59,79,99,119,141 --only-assembler -o '+assembly_abs+''
+        assemblyCmd = 'module load anaconda3/2.1.0 spades/3.13.1 perl/5.20.2 && rm -rf '+assembly_abs+' && metaspades.py -1 '+read1in+' -2 '+read2in+' -t '+threads+' -m '+memory+' -k 21,29,39,59,79,99,119,141 --only-assembler -o '+assembly_abs+''
     if assembler == 'megahit':
-        assemblyCmd = 'module load tools megahit/1.1.1 && megahit -1 '+read1in+' -2 '+read2in+' -t '+threads+' -m '+memory+' --k-list 21,29,39,59,79,99,119,141 -o '+assembly_abs+''
+        assemblyCmd = 'module load tools megahit/1.1.1 && rm -rf '+assembly_abs+' && megahit -1 '+read1in+' -2 '+read2in+' -t '+threads+' -m '+memory+' --k-list 21,29,39,59,79,99,119,141 -o '+assembly_abs+''
 
     #Run assembly
     subprocess.check_call(assemblyCmd, shell=True)
