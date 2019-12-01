a210f3790dcaa69e55cf6a51ae82f910b928fd91
“anttonalberdi” <anttonalberdi@gmail.com>
2019-12-01 08:05:32 +0100

    v

--- metafunk2/assembly.py
+++ metafunk2/assembly.py
@@ -23,8 +23,8 @@ def assembly(outpath,name,logfilepath,statsfilepath,threads,memory,keep):
     if not os.path.exists(assembly_abs):
         os.makedirs(assembly_abs)
     #Declare input files
-    read1in = os.path.join(outpath, name +  '.1.fq')
-    read2in = os.path.join(outpath, name +  '.2.fq')
+    read1in = os.path.join(outpath, name +  '.1.fq.gz')
+    read2in = os.path.join(outpath, name +  '.2.fq.gz')
 
     #Run assembly
     assemblyCmd = 'module load anaconda3/2.1.0 spades/3.13.1 perl/5.20.2 && metaspades.py -1 '+read1in+' -2 '+read2in+' -t '+threads+' -m '+memory+' -k 21,29,39,59,79,99,119 --only-assembler -o '+assembly_abs+''
