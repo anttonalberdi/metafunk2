e064e898bf379e58693ac774569539094f5b6d53
nuriaher <57942941+nuriaher@users.noreply.github.com>
2020-01-17 13:25:57 +0100

    Update binning_merged.py

--- metafunk2/binning_merged.py
+++ metafunk2/binning_merged.py
@@ -46,7 +46,7 @@ def binning_merged(projectname,projectpath,threads,memory,logfilepath):
     current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
     logfile.write("{0} |    Running metabat binning\r\n".format(current_time))
     logfile.close()
-    metabatCmd = 'module unload gcc && module load tools perl/5.20.2 metabat/2.12.1 && metabat2 -i '+reassemblypath+' -a '+metabatdepthfile+' -o '+metabatbinbase+' -m 1500 -t '+threads+'' #added tools
+    metabatCmd = 'module unload gcc && module load perl/5.20.2 metabat/2.12.1 && metabat2 -i '+reassemblypath+' -a '+metabatdepthfile+' -o '+metabatbinbase+' -m 1500 -t '+threads+'' 
     subprocess.check_call(metabatCmd, shell=True)
 
     #Create contig to bin table
